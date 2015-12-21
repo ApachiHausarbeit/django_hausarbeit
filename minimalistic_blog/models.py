475c88c90bbd2e289ec5a51f96ce4cb383665613
Ibrahim Apachi <i.apachi00@gmail.com>
2015-12-21 09:43:16 +0100

    Ein Blog und ein Comment Model wurden erstellt. Beide haben einen ForeignKey zum Base User. Nach der Erstellung der Models wurden folgende Befehle ausgeführt um diese Änderungen wirkhaft zu machen. python manage.py makemigrations um die Migrationen vorzubereiten und python manage.py migrate um die Datenbank zu migrieren.

--- minimalistic_blog/models.py
+++ minimalistic_blog/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=80, unique=True)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User)
+    blog = models.ForeignKey(Blog)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
